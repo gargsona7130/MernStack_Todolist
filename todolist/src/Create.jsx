4f30193cafa9611c36775e47084ab89404cdfb28
gargsona7130 <gargsonal7130@gmail.com>
2024-06-20 11:14:13 +0530

    Basic todo created

--- todolist/src/Create.jsx
+++ todolist/src/Create.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 function Create(){
+    const [task, setTask ] = useState()
+    const handleAdd = () => {
+        axios.post('https://localhost:3001/add',{task:task})
+        .then(result => console.log(result))
+        .catch(err => console.log(err))
+    }
+
     return(
-        <div> 
+        <div className='create-form'> 
             <input type="text" placeholder='Enter Task' onChange={(e) => setTask(e.target.value)} />
             <button type="button" onClick={handleAdd}> Add </button>
         </div>
--- todolist/src/Home.jsx
+++ todolist/src/Home.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React,{ useState } from 'react'
+import Create from './Create'
+import './App.css';
 
 function Home(){
     const [todos, setTodos ] = useState([])
     return(
-        <div> 
+        <div className="main-container"> 
             <h2> Todo List </h2>
             <Create />
             {
-                 todos.map(todo => (
+                todos.length === 0
+                ?
+                <div> <h2> No Record </h2> </div>
+                :
+                todos.map(todo => (
                     <div>
                         {todo}
                         </div>
